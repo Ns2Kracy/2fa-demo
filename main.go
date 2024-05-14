a7b990c8e6bcba24790e94d3f56f12870feffa25
Ns2Kracy <ns2kracy@gmail.com>
2024-05-14 14:44:52 +0800

    generate base64 qrcode

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/pquerna/otp/totp"
+	"github.com/skip2/go-qrcode"
 )
 
 func main() {
@@ -27,9 +29,17 @@ func Generate2FA(c echo.Context) error {
 
 	otpURL := key.URL()
 
+	png, err := qrcode.Encode(otpURL, qrcode.Medium, 256)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	encodingPng := base64.StdEncoding.EncodeToString(png)
+
 	return c.JSON(http.StatusOK, map[string]string{
 		"secret": key.Secret(),
 		"url":    otpURL,
+		"qrcode": "data:image/png;base64," + encodingPng,
 	})
 }
 
